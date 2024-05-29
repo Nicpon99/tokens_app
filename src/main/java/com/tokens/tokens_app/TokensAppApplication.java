e9d336eb39629468ca0c2efc8662f3a0ed6f4308
Nicpon99 <Nicpon044@onet.pl>
2024-05-29 17:40:14 +0200

    second commit

---
+++ src/main/java/com/tokens/tokens_app/TokensAppApplication.java
@@ -0,0 +1,13 @@
+package com.tokens.tokens_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TokensAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TokensAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tokens/tokens_app/TokensAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tokens.tokens_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TokensAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
